1ac4548486820bdb2626c228726448b58e6e0bc4
yalecheung <yalecheung@outlook.com>
2019-02-12 15:32:05 +0800

    fix bugs

---
+++ test/exlcm/publisher.cc
@@ -0,0 +1,39 @@
+#include "example_t.hpp"
+#include "../../include/lcm_node.h"
+#include <stdio.h>
+
+
+
+using example_t = exlcm::example_t;
+
+typedef void(* Callback_t)(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const example_t* msg, void* contex);
+int main(int argc, char* argv[]) {
+    auto comm = std::make_shared<LCMNode<example_t, Callback_t>>();
+    example_t my_data;
+
+    int* pint = new int;
+    *pint = 2;
+
+    my_data.timestamp = 0;
+    my_data.position[0] = 1;
+    my_data.position[1] = 2;
+    my_data.position[2] = 3;
+   
+    my_data.orientation[0] = 1;
+    my_data.orientation[1] = 0;
+    my_data.orientation[2] = 0;
+    my_data.orientation[3] = 0;
+   
+    my_data.num_ranges = 15;
+    my_data.ranges.resize(my_data.num_ranges);
+    for(int i = 0; i < my_data.num_ranges; i++)
+        my_data.ranges[i] = i;
+   
+    my_data.name = "example string";
+    my_data.enabled = true;
+   
+    printf("start publish\n");
+    comm->publish("EXAMPLE", my_data);
+    printf("end publish\n");
+    return 0;
+}
--- test/exlcm/subscriber.cc
+++ test/exlcm/subscriber.cc
@@ -1,53 +1,57 @@
-// file: listener.cpp
-//
-// LCM example program.
-//
-// compile with:
-//  $ gcc -o listener listener.cpp -llcm
-//
-// On a system with pkg-config, you can also use:
-//  $ gcc -o listener listener.cpp `pkg-config --cflags --libs lcm`
-
+#include "example_t.hpp"
+#include "../../include/lcm_node.h"
 #include <stdio.h>
 
-#include <lcm/lcm-cpp.hpp>
-#include "example_t.hpp"
 
-class Handler {
-  public:
-    ~Handler() {}
-    void handleMessage(const lcm::ReceiveBuffer *rbuf, const std::string &chan,
-                       const exlcm::example_t *msg)
-    {
+
+using example_t = exlcm::example_t;
+
+typedef void(* Callback_t)(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const example_t* msg, void* contex);
+int main(int argc, char* argv[]) {
+    auto comm = std::make_shared<LCMNode<example_t, Callback_t>>();
+    example_t my_data;
+
+    int* pint = new int;
+    *pint = 2;
+
+    my_data.timestamp = 0;
+    my_data.position[0] = 1;
+    my_data.position[1] = 2;
+    my_data.position[2] = 3;
+   
+    my_data.orientation[0] = 1;
+    my_data.orientation[1] = 0;
+    my_data.orientation[2] = 0;
+    my_data.orientation[3] = 0;
+   
+    my_data.num_ranges = 15;
+    my_data.ranges.resize(my_data.num_ranges);
+    for(int i = 0; i < my_data.num_ranges; i++)
+        my_data.ranges[i] = i;
+   
+    my_data.name = "example string";
+    my_data.enabled = true;
+   
+     
+    auto callback = [](const lcm::ReceiveBuffer* rbuf, const std::string& chan, const example_t* msg, void* ptr) {
+        printf("%x\n", (int*)ptr);
         int i;
         printf("Received message on channel \"%s\":\n", chan.c_str());
-        printf("  timestamp   = %lld\n", (long long) msg->timestamp);
-        printf("  position    = (%f, %f, %f)\n", msg->position[0], msg->position[1],
-               msg->position[2]);
-        printf("  orientation = (%f, %f, %f, %f)\n", msg->orientation[0], msg->orientation[1],
-               msg->orientation[2], msg->orientation[3]);
+        printf("  timestamp   = %lld\n", (long long)msg->timestamp);
+        printf("  position    = (%f, %f, %f)\n",
+                msg->position[0], msg->position[1], msg->position[2]);
+        printf("  orientation = (%f, %f, %f, %f)\n",
+                msg->orientation[0], msg->orientation[1], 
+                msg->orientation[2], msg->orientation[3]);
         printf("  ranges:");
-        for (i = 0; i < msg->num_ranges; i++)
+        for(i = 0; i < msg->num_ranges; i++)
             printf(" %d", msg->ranges[i]);
         printf("\n");
         printf("  name        = '%s'\n", msg->name.c_str());
         printf("  enabled     = %d\n", msg->enabled);
-    }
-};
-
-int main(int argc, char **argv)
-{
-    lcm::LCM lcm("udpm://...");
-
-    if (!lcm.good())
-        return 1;
-
-    Handler handlerObject;
-    lcm.subscribe("EXAMPLE", &Handler::handleMessage, &handlerObject);
-
-    while (0 == lcm.handle()) {
-        // Do nothing
-    }
-
+    };
+    printf("start sub\n");
+    comm->subscribe("EXAMPLE", callback, (void*)pint);
+    comm->start();
     return 0;
 }
