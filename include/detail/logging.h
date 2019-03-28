547f5c81ea0c49e998cb5aab29fe54fe92c57c72
yalecheung <yalecheung@outlook.com>
2019-03-28 17:15:55 +0800

    log

--- include/detail/logging.h
+++ include/detail/logging.h
@@ -3,8 +3,13 @@
 
 #include <thread>
 
+
 class Logging {
+    
 public:
+
+    
+
     enum LogLevel { DEBUG, INFO, WARN, ERROR, FATAL, LEVEL_NUM };
 
 private:
@@ -16,13 +21,23 @@ private:
           
         }
     private:
-        std::chrono::time_point _tp;
-        std::thread::id
-        LogLevel _level;
-        int _line
-        FilePath _path;
 
     }
+    class LogPreamble : NonCopyable {
+    public: 
+        LogPreamble() {}
+
+        void addDate()       {_buf.append("%04d-%02d-%02d ", 15);}
+        void addTime()       {_buf.append("%02d:%02d:%02d.%03lld ", 23);}
+        void addUpTime()     {_buf.append("(%6.3f) ", 8);}
+        void addTid()        {_buf.append("%-*s ", 5);}
+        void addFileInfo()   {_buf.append("%*s:%-5u ", 9);}
+        void addLogLevel()   {_buf.append("%4s ", 4);}
+        void addSplitter()   {_buf.append("%1s ", 1);}
+    private:
+        Buffer _buf;
+    }
+    
     Impl _imp;
 };
 
